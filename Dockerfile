df7a826940240d41043de3b4b308ded29b489186
angelazzhou110-ctrl <angela.zzhou110@gmail.com>
2026-02-21 15:09:10 -0500

    finished demo

--- Dockerfile
+++ Dockerfile
@@ -1,1 +1,12 @@
-d
+FROM python:3.15.0a6-trixie
+
+WORKDIR /app
+
+COPY requirements.txt .
+
+RUN pip install --no-cache-dir -r requirements.txt
+COPY app.py /app 
+
+EXPOSE 800
+
+CMD ["python", "app.py"]
--- app.py
+++ app.py
@@ -1,1 +1,14 @@
-#
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.get("/")
+def hello():
+    return "Hello from me!"
+
+@app.get("/about")
+def about():
+    return "Read me!"
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port = 800)
